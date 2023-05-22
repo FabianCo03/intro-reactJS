de4d3f34041c3a3cabe9fa87ef0ed4564f5e7ea9
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-05-22 18:02:20 -0500

    feat: 🎸 Clase 07: introducción a estados: useState

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./styles/TodoSearch.css";
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+  console.log("los usuarios buscan ToDO'S de " + searchValue);
   return (
     <input
       className="TodoSearch"
-      placeholder="Hacer ejercicio"
+      placeholder="Search"
+      value={searchValue}
       onChange={(event) => {
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
       }}
     ></input>
   );
