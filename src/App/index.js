574ae7f364af604a280fd372460efd45af405bed
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-05-30 12:23:02 -0500

    feat: 🎸 Clase 17: Tips para naming y abstracción de componentes

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,6 @@
 import React from "react";
-import { TodoCounter } from "../TodoCounter";
-import { TodoSearch } from "../TodoSearch";
-import { TodoList } from "../TodoList";
-import { TodoItem } from "../TodoItem";
-import { CreateTodoButton } from "../CreateTodoButton/index";
+import { AppUI } from "./AppUI";
 import { useLocalStorage } from "./useLocalStorage";
-
-// const defaultTodos = [
-//   { text: "Hacer trabajos", completed: false },
-
-//   { text: "Jugar fulvol", completed: false },
-
-//   { text: "Comer", completed: false },
-// ];
-
-// localStorage.setItem('TODOS_v1', JSON.stringify(defaultTodos))
-// localStorage.removeItem('TODOS_v1')
 
 function App() {
   const [todos, saveTodos] = useLocalStorage("TODOS_v1", []);
@@ -48,27 +33,27 @@ function App() {
   };
 
   return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos} />
-
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            // key es cuando queremos renderizar distintos elementos a partir de un array
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </>
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
 export default App;
+
+// const defaultTodos = [
+//   { text: "Hacer trabajos", completed: false },
+
+//   { text: "Jugar fulvol", completed: false },
+
+//   { text: "Comer", completed: false },
+// ];
+
+// localStorage.setItem('TODOS_v1', JSON.stringify(defaultTodos))
+// localStorage.removeItem('TODOS_v1')
